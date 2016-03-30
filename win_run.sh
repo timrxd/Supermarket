8ac66d1d65bc54902f7f041cb5686572b91676b0
timrxd <timxdowd@gmail.com>
2016-03-30 15:19:25 -0400

    For Windows

--- win_run.sh
+++ win_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 javac -d bin/main/java/ src/main/java/*.java  
-java -cp ./bin/main/java/;./lib/* main.java.RegisterDriver
+java -cp "./bin/main/java/;./lib/*" main.java.RegisterDriver
